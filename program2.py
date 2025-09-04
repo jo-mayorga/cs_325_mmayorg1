d750cef685d2e99350c6c3964de2d76401e886f5
Tlaloc1 <Supernova1@outlook.com>
2025-09-04 10:16:16 -0500

    Update program2.py

--- program2.py
+++ program2.py
@@ -4,7 +4,7 @@ letter=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R",
 def writeSequence():
     pickedLetter = letter[random.randint(0,25)]
     pickedNumber= random.randint(100,999)
-    return {f'{pickedLetter}{pickedNumber}'}
+    return f'{pickedLetter}{pickedNumber}'
 
-for i in range(0,10):
-    print(writeSequence())
+for i in range(0,100):
+    print(writeSequence(),end='/')
